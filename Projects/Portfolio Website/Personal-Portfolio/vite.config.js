f64e06a3b2f7a1f22b089940da20d5385a30d71a
Ritesh Sutrave <rsutrave7@gmail.com>
2025-03-09 20:57:10 +0530

    Intial commit

--- Projects/Portfolio Website/Personal-Portfolio/vite.config.js
+++ Projects/Portfolio Website/Personal-Portfolio/vite.config.js
@@ -1,15 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'src/main.jsx', // Or 'src/index.jsx' or 'src/index.js' depending on your entry file
-      },
-    },
-  },
 })
